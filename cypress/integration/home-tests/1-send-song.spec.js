730f4eae44b2e042981302b2e1addf6aec043b85
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-05-05 00:10:54 -0300

    chore/feat: configured cypress, added '/home - sendSong' basic test

---
+++ cypress/integration/home-tests/1-send-song.spec.js
@@ -0,0 +1,17 @@
+const BASE_URL = "http://localhost:3000";
+
+const songA = {
+  name: 'Ponto de Vista',
+  youtubeLink: 'https://www.youtube.com/watch?v=1dmQmMUdMt8',
+};
+
+describe("/home - send a song", () => {
+  it("should succesfully send a new song", () => {
+    cy.visit(BASE_URL);
+
+    cy.get('input[id="songName"]').type(songA.name);
+    cy.get('input[id="songLink"]').type(songA.youtubeLink);
+    cy.get('button[id="sendSong"]').click();
+
+  });
+});
