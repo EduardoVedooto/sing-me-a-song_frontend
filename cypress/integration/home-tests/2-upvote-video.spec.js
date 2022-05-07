059df5a6d772b7c2a9d7e14e8b19a1dbd8710437
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-05-07 18:02:57 -0300

    click 'upVoting' video by title seems to be working

--- cypress/integration/home-tests/2-upvote-video.spec.js
+++ cypress/integration/home-tests/2-upvote-video.spec.js
@@ -1,17 +1,32 @@
 const BASE_URL = 'http://localhost:3000';
 
+const songB = {
+  name: 'Foo Fighters: The Pretender',
+  youtubeLink: 'https://www.youtube.com/watch?v=SBjQ9tuuTJQ',
+};
+
 describe('/home - upvote a video', () => {
   it('should succesfully upvote in a video', () => {
     cy.visit(BASE_URL);
+
+    /*
+    cy.request('POST', 'http://localhost:5000/recommendations', songB).then((response) => {
+      console.log(response);
+    });
+    */
+
     cy.intercept('GET', '/recommendations').as('getRecommendations');
     cy.wait('@getRecommendations');
 
-    cy.get('[id="reactPlayer"]').should('be.visible');
-    // mudar modo de busca: id deve ser único na página
+    cy.get('article').contains(songB.name).should('be.visible');
 
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(750);
-    cy.get('[id="upvoteArrow"]').click();
+    cy.get('article').contains(songB.name)
+      .parent()
+      .find('div:nth-child(3)')
+      .find('svg:nth-child(1)')
+      .click();
+
+    // cy.get('article div:nth-child(3) svg:nth-child(1)').click();
   });
 });
 
